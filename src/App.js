80d3fa275c3c43b5223df4de4aff3577f57eadae
bigveloper <bigveloper@gmail.com>
2021-09-21 23:19:42 +0900

    textArea

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 // Component
 function App() {
     // State
+    const [value, setValue] = useState('');
 
     // Event
+    const onChange = (e) => setValue(e.target.value);
 
     // Watch
-
+    useEffect(() => {
+        console.log(value);
+    }, [value]);
     // JSX
     return (
         <div className="App">
             <h1>TextArea</h1>
+            <textarea value={value} onChange={onChange} />
         </div>
     );
 }
