8dbf33284bda32d37124b072b3d11495e52c71ea
bigveloper <bigveloper@gmail.com>
2021-09-21 22:48:26 +0900

    test1 useEffect To Do List

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 // Component
 function App() {
     // State
+    const [value, setValue] = useState('');
 
     // Event
+    const onChange = (e) => setValue(e.target.value);
 
     // Watch
 
     // JSX
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h1>To Do List</h1>
+            <input value={value} onChange={onChange} />
+        </div>
+    );
 }
 
 export default App;
