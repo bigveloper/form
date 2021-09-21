a04d207dfc212c19d137abb5969f821eb6e602b2
bigveloper <bigveloper@gmail.com>
2021-09-22 00:11:22 +0900

    Select

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     // State
     const [value, setValue] = useState('');
     const [fileUp, setFileUp] = useState('');
+    const [selected, setSelected] = useState([]);
+    const itCompany = ['FaceBook', 'MS', 'Apple', 'Aamazon', 'Hancom'];
 
     // Event
     const onChange = (e) => setValue(e.target.value);
@@ -13,6 +15,7 @@ function App() {
         setValue('');
     };
     const onFile = (e) => setFileUp(e.target.value);
+    const onSelect = (e) => setSelected(e.target.selected);
 
     // Watch
     useEffect(() => {
@@ -22,6 +25,7 @@ function App() {
     useEffect(() => {
         console.log(fileUp);
     }, [fileUp]);
+
     // JSX
     return (
         <div className="App">
@@ -37,6 +41,15 @@ function App() {
             <hr />
             <h1> File Upload </h1>
             <input type="file" value={fileUp} onChange={onFile} />
+            <hr />
+            <h1> SelectBox </h1>
+            <select value={selected} onSelect={onSelect}>
+                {itCompany.map((item) => (
+                    <option value={item} key={item}>
+                        {item}
+                    </option>
+                ))}
+            </select>
         </div>
     );
 }
