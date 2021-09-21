0399c0060280f5fc10b6c3011b359d126f7c39d1
bigveloper <bigveloper@gmail.com>
2021-09-21 22:59:17 +0900

    test2 useEffect To Do List

--- src/App.js
+++ src/App.js
@@ -5,17 +5,47 @@ import './App.css';
 function App() {
     // State
     const [value, setValue] = useState('');
-
+    const [list, setList] = useState([]);
     // Event
     const onChange = (e) => setValue(e.target.value);
+    const onClick = () => {
+        if (!value) {
+            alert('write your to do');
+        }
+        setList((prevState) => [...prevState, value]);
+        setValue('');
+    };
+    const onEdit = (index) => {
+        console.log(index);
 
+        const editText = prompt();
+        console.log(editText);
+
+        setList((prevState) => {
+            const editList = prevState.map((item, i) => {
+                return i === index ? editText : item;
+            });
+            return editList;
+        });
+    };
     // Watch
+    useEffect(() => {
+        console.log(value);
+    }, [value]);
 
     // JSX
     return (
         <div className="App">
             <h1>To Do List</h1>
             <input value={value} onChange={onChange} />
+            <button onClick={onClick}>ADD</button>
+            <hr />
+            {list.map((item, index) => (
+                <div key={index}>
+                    <input value={item} readOnly />
+                    <button onClick={() => onEdit(index)}>Edit</button>
+                </div>
+            ))}
         </div>
     );
 }
