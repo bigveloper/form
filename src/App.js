4289b72833a42952a95d1e732976cfdfed3df92e
bigveloper <bigveloper@gmail.com>
2021-09-21 23:14:27 +0900

    all Delete

--- src/App.js
+++ src/App.js
@@ -1,65 +1,17 @@
-import { useState, useEffect } from 'react';
 import './App.css';
 
 // Component
 function App() {
     // State
-    const [value, setValue] = useState('');
-    const [list, setList] = useState([]);
+
     // Event
-    const onChange = (e) => setValue(e.target.value);
-    const onClick = () => {
-        if (!value) {
-            alert('write your to do');
-        }
-        setList((prevState) => [...prevState, value]);
-        setValue('');
-    };
-    const onEdit = (index) => {
-        console.log(index);
 
-        const editText = prompt();
-        console.log(editText);
-
-        setList((prevState) => {
-            const editList = prevState.map((item, i) => {
-                return i === index ? editText : item;
-            });
-            return editList;
-        });
-    };
-
-    const onDelete = (index) => {
-        console.log(index);
-        setList((prevState) => {
-            const deleteList = prevState.filter((item, i) => {
-                return i !== index && item;
-            });
-            return deleteList;
-        });
-    };
     // Watch
-    useEffect(() => {
-        console.log(value);
-    }, [value]);
 
-    useEffect(() => {
-        console.log(list);
-    }, [list]);
     // JSX
     return (
         <div className="App">
-            <h1>To Do List</h1>
-            <input value={value} onChange={onChange} />
-            <button onClick={onClick}>ADD</button>
-            <hr />
-            {list.map((item, index) => (
-                <div key={index}>
-                    <input value={item} readOnly />
-                    <button onClick={() => onEdit(index)}>Edit</button>
-                    <button onClick={() => onDelete(index)}>Delete</button>
-                </div>
-            ))}
+            <h1>TextArea</h1>
         </div>
     );
 }
