003be4dfd3469d30ec738e876a9cd1de0f2962e1
bigveloper <bigveloper@gmail.com>
2021-09-22 21:23:52 +0900

    checkBox

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+function App() {
+    const [laptop, setLaptop] = useState(false);
+
+    const onChange = (e) => setLaptop(e.target.value);
+
+    useEffect(() => {
+        console.log(laptop);
+    }, [laptop]);
+    return (
+        <form className="App">
+            <h1>What do you want to laptop??</h1>
+            <input type="checkbox" name="laptop" value="MacBook Air" onChange={onChange} /> Apple MacBook Air <br />
+            <input type="checkbox" name="laptop" value="MacBook Pro" onChange={onChange} /> Apple MacBook Pro <br />
+            <input type="checkbox" name="laptop" value="Galaxy Book" onChange={onChange} /> Samsung Galaxy Book <br />
+            <input type="checkbox" name="laptop" value="Gram" onChange={onChange} /> LG Gram <br />
+            <input type="checkbox" name="laptop" value="ZenBook" onChange={onChange} /> Asus ZenBook <br />
+        </form>
+    );
+}
+
+export default App;
