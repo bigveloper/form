4fd00da3979ebf11e6465f777a28df7cf1802e44
bigveloper <bigveloper@gmail.com>
2021-09-22 12:19:38 +0900

    I'm not sure if select works.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import './App.css';
 // Component
 function App() {
     // State
+    const [tdSelect, setTdSelect] = useState('');
+    const languageList = ['JavaScript', 'HTML', 'CSS'];
 
     // Event
+    const tdClick = (e) => {
+        setTdSelect(e.target.value);
+        console.log(tdSelect);
+        return;
+    };
 
     // watch
+
+    useEffect(() => {
+        console.log(tdSelect);
+    }, [tdSelect]);
 
     //JSX
     return (
         <div>
-            <></>
+            <p>select box</p>
+            <select value={tdSelect} onChange={tdClick}>
+                {languageList.map((item) => (
+                    <option value={item} key={item}>
+                        {item}
+                    </option>
+                ))}
+            </select>
         </div>
     );
 }
