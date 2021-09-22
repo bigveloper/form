20a1da5795f1367a04b21e7c5b51eef9f7dbb520
bigveloper <bigveloper@gmail.com>
2021-09-22 15:54:21 +0900

    Radio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 // Component
 function App() {
     // State
+    const [isRadio, setIsRadio] = useState(false);
 
     // Event
+    const clickOnRadio = () => {
+        console.log(!isRadio);
+        setIsRadio(!isRadio);
+    };
 
     // watch
+    useEffect(() => {
+        console.log();
+    }, [isRadio]);
 
     //JSX
     return (
         <div>
-            <></>
+            <p> Radio </p>
+            <input type="radio" value={isRadio} id="radio" onChange={clickOnRadio} />
         </div>
     );
 }
