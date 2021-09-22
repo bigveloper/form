f3f775543496f8838852f41d0b562bf870665448
bigveloper <bigveloper@gmail.com>
2021-09-22 15:25:52 +0900

    Delete

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
-import { useState, useEffect } from 'react';
 import './App.css';
 
 // Component
 function App() {
     // State
-    const [tdSelect, setTdSelect] = useState('');
-    const languageList = ['JavaScript', 'HTML', 'CSS', 'Java', 'Python'];
 
     // Event
-    // const tdClick = (e) => {
-    //     setTdSelect(e.target.value);
-    //     return;
-    // };
-    const onSubmit = (e) => {
-        setTdSelect(e.target.value);
-        e.preventDefault();
-    };
 
     // watch
 
-    useEffect(() => {
-        console.log(tdSelect);
-    }, [tdSelect]);
-
     //JSX
     return (
-        <form onSubmit={onSubmit}>
-            <p>select box</p>
-            <select value={tdSelect} onChange={onSubmit}>
-                {languageList.map((item) => (
-                    <option value={item} key={item}>
-                        {item}
-                    </option>
-                ))}
-            </select>
-            <input type="submit" value="Submit" />
-        </form>
+        <div>
+            <></>
+        </div>
     );
 }
 
