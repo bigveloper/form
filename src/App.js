0913afecddf5b7becefd06445b79a25218d5bd41
bigveloper <bigveloper@gmail.com>
2021-09-22 11:10:54 +0900

    other way Radio

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 // Component
 function App() {
+    const [laptop, setLaptop] = useState('');
+
     // State
 
     // Event
@@ -10,7 +12,23 @@ function App() {
     // Watch
 
     // JSX
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <label>Laptop</label>
+            <input type="radio" value="MacBook Air" name={laptop} checked={laptop === 'MacBook Air'} onClick={() => setLaptop('MacBook Air')} />
+        </div>
+    );
 }
 
 export default App;
+
+// const ListCreateNew = () => {
+//     const [gender, setGender] = useState('Male');
+//     return (
+//         <Form.Group inline>
+//             <label>Gender</label>
+//             <Form.Field control={Radio} label="Male" checked={gender === 'Male'} value="Male" onClick={() => setGender('Male')} />
+//             <Form.Field control={Radio} label="Female" checked={gender === 'Female'} value="Female" onClick={() => setGender('Female')} />
+//         </Form.Group>
+//     );
+// };
