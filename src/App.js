7a5ac530bbb76c5339dd06f6bd4c6bc7ef3489d8
bigveloper <bigveloper@gmail.com>
2021-09-22 21:27:05 +0900

    checkbox + submit + reset

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ function App() {
             <input type="checkbox" name="laptop" value="Galaxy Book" onChange={onChange} /> Samsung Galaxy Book <br />
             <input type="checkbox" name="laptop" value="Gram" onChange={onChange} /> LG Gram <br />
             <input type="checkbox" name="laptop" value="ZenBook" onChange={onChange} /> Asus ZenBook <br />
+            <input type="submit" value="전송" />
+            <input type="reset" value="Reset" />
         </form>
     );
 }
