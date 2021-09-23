cbee90f6cc7b3d7be5953bf9aa9afe6f8e64e161
bigveloper <bigveloper@gmail.com>
2021-09-23 22:11:49 +0900

    checkbox

--- src/App.js
+++ src/App.js
@@ -11,10 +11,20 @@ function App() {
 
     return (
         <form>
-            <h1> what is your favorite Camera brand</h1>
+            <h1> what is your favorite Camera brand??</h1>
             <input type="radio" name="camera" value="Sony" onChange={onChange} /> Sony <br />
             <input type="radio" name="camera" value="Cannon" onChange={onChange} /> Cannon <br />
             <input type="radio" name="camera" value="Nikon" onChange={onChange} /> Nikon <br />
+            <input type="radio" name="camera" value="Pannasonic" onChange={onChange} /> Pannasonic <br />
+            <input type="submit" />
+            <input type="reset" />
+            <hr />
+            <h1>what is your favorite laptop??</h1>
+            <input type="checkbox" name="laptop" value="MacBookAir" onChange={onChange} /> MacBook Air <br />
+            <input type="checkbox" name="laptop" value="MacBookPro" onChange={onChange} /> MacBook Pro <br />
+            <input type="checkbox" name="laptop" value="GalaxyBook" onChange={onChange} /> Galaxy Book <br />
+            <input type="checkbox" name="laptop" value="ZenBook" onChange={onChange} /> ZenBook <br />
+            <input type="checkbox" name="laptop" value="Gram" onChange={onChange} /> Gram <br />
             <input type="submit" />
             <input type="reset" />
         </form>
