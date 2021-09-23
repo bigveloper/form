e69c76278ce580151edf8855ca197cb720baf4db
bigveloper <bigveloper@gmail.com>
2021-09-23 21:27:03 +0900

    TodoList Edit

--- src/App.js
+++ src/App.js
@@ -15,6 +15,23 @@ function App() {
         setList((prevState) => [...prevState, value]);
         setValue('');
     };
+    const onEdit = (index) => {
+        const editText = prompt();
+        setList((prevState) => {
+            const editList = prevState.map((item, i) => {
+                return i === index ? editText : item;
+            });
+            return editList;
+        });
+    };
+
+    useEffect(() => {
+        console.log(value);
+    }, [value]);
+
+    useEffect(() => {
+        console.log(list);
+    }, [list]);
 
     // Watch
 
@@ -27,6 +44,7 @@ function App() {
             {list.map((item, index) => (
                 <div key={index}>
                     <input value={item} readOnly />
+                    <button onClick={() => onEdit(index)}>Edit</button>
                 </div>
             ))}
         </div>
