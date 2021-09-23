b21b7587915cb4f77c2f6d8fcc17b0ae36861225
bigveloper <bigveloper@gmail.com>
2021-09-23 21:51:46 +0900

    radio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import { useState, useEffect } from 'react';
 
 function App() {
+    const [camera, setCamera] = useState('');
+
+    const onChange = (e) => setCamera(e.target.camera);
+
+    useEffect(() => {
+        console.log(camera);
+    }, [camera]);
+
     return (
         <form>
-            <></>
+            <h1> what is your favorite Camera brand</h1>
+            <input type="radio" name="camera" value="Sony" onChange={onChange} /> Sony
+            <input type="radio" name="camera" value="Cannon" onChange={onChange} /> Cannon
+            <input type="radio" name="camera" value="Nikon" onChange={onChange} /> Nikon
         </form>
     );
 }
