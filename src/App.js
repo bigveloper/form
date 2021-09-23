d610a26e76d6991bb01b458c4d8ed5671b075ac2
bigveloper <bigveloper@gmail.com>
2021-09-23 21:17:55 +0900

    ToDolist

--- src/App.js
+++ src/App.js
@@ -5,15 +5,32 @@ import './App.css';
 function App() {
     // State
     const [value, setValue] = useState('');
+    const [list, setList] = useState([]);
     // Evnet
     const onChange = (e) => setValue(e.target.value);
+    const onClick = () => {
+        if (!value) {
+            alert('write something');
+        }
+        setList((prevState) => [...prevState, value]);
+        setValue('');
+    };
 
     // Watch
 
     //JSX
     return (
-        <div>
+        <div className="App">
+            <h1>To Do List</h1>
             <input value={value} onChange={onChange} />
+            <button onClick={onClick}>ADD</button>
+            {list.map((item, index) => (
+                <div key={index}>
+                    <input value={item} readOnly />
+                </div>
+            ))}
         </div>
     );
 }
+
+export default App;
