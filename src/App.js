34c109da212107a287646d6208c8a7888cd887cd
bigveloper <bigveloper@gmail.com>
2021-09-23 22:22:14 +0900

    checkbox

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import { useState, useEffect } from 'react';
 
 function App() {
     const [camera, setCamera] = useState('');
+    const [laptop, setLaptop] = useState('');
 
-    const onChange = (e) => setCamera(e.target.camera);
+    const onChange = (e) => setCamera(e.target.value);
+    const onCheck = (e) => setLaptop(e.target.value);
 
     useEffect(() => {
         console.log(camera);
     }, [camera]);
+
+    useEffect(() => {
+        console.log(laptop);
+    }, [laptop]);
 
     return (
         <form>
@@ -20,11 +26,11 @@ function App() {
             <input type="reset" />
             <hr />
             <h1>what is your favorite laptop??</h1>
-            <input type="checkbox" name="laptop" value="MacBookAir" onChange={onChange} /> MacBook Air <br />
-            <input type="checkbox" name="laptop" value="MacBookPro" onChange={onChange} /> MacBook Pro <br />
-            <input type="checkbox" name="laptop" value="GalaxyBook" onChange={onChange} /> Galaxy Book <br />
-            <input type="checkbox" name="laptop" value="ZenBook" onChange={onChange} /> ZenBook <br />
-            <input type="checkbox" name="laptop" value="Gram" onChange={onChange} /> Gram <br />
+            <input type="checkbox" name="laptop" value="MacBookAir" onChange={onCheck} /> MacBook Air <br />
+            <input type="checkbox" name="laptop" value="MacBookPro" onChange={onCheck} /> MacBook Pro <br />
+            <input type="checkbox" name="laptop" value="GalaxyBook" onChange={onCheck} /> Galaxy Book <br />
+            <input type="checkbox" name="laptop" value="ZenBook" onChange={onCheck} /> ZenBook <br />
+            <input type="checkbox" name="laptop" value="Gram" onChange={onCheck} /> Gram <br />
             <input type="submit" />
             <input type="reset" />
         </form>
