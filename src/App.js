bc51586dd70179067405a2e9238686a27202758e
bigveloper <bigveloper@gmail.com>
2021-09-26 02:35:12 +0900

    checkbox

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState, useEfect } from 'react';
+import { useEffect } from 'react/cjs/react.development';
+
+function App() {
+    const [laptop, setLaptop] = useState('');
+
+    const onChange = (e) => setLaptop(e.target.value);
+
+    useEffect(() => {
+        console.log(laptop);
+    }, [laptop]);
+
+    return (
+        <form>
+            <h1>What do you want to laptop?</h1>
+            <input type="checkbox" name="laptop" value="MacBookAir" onChange={onChange} />
+            MacBook Air
+            <br />
+            <input type="checkbox" name="laptop" value="MacBookPro" onChange={onChange} />
+            MacBook Pro
+            <br />
+            <input type="checkbox" name="laptop" value="GalaxyBook" onChange={onChange} />
+            GalaxyBook
+            <br />
+            <input type="checkbox" name="laptop" value="MacBookAir" onChange={onChange} />
+            ZenBook
+            <br />
+            <input type="submit" />
+            <br />
+            <input type="reset" />
+        </form>
+    );
+}
+
+export default App;
